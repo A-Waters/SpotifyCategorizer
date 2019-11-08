c976ae76259ba73b766ac4e515626eb60a229f34
Alexnader Watermen <AlexanderWatermenAWC@gmail.com>
2019-11-08 18:16:11 -0500

    WE HAVE ACCESS

--- main.py
+++ main.py
@@ -7,7 +7,17 @@ if __name__ == "__main__":
     SpoClient = spotify.spotifyClient()
     SpoClient.authorize()
     
-    
 
 
+    usertracks = "/v1/me/tracks"
+    audio_analisys = "/v1/audio-analysis/"#ID
 
+    tempQuery = {"limit": "5"}
+    result = SpoClient.getData(usertracks, tempQuery)
+
+    '''for i in result["items"]:
+        print(i["track"]["id"])'''
+
+    uhhh = SpoClient.getData(audio_analisys+"7LJzs1Kr8kW3d6cFTCueNV")
+    print(uhhh)
+ 	
--- spotify.py
+++ spotify.py
@@ -5,6 +5,7 @@ import time
 import pickle
 import os.path
 import base64
+import json
 from selenium import webdriver
 
 
@@ -14,17 +15,13 @@ class spotifyClient():
     REDIR_URL = "https://example.com/callback/"    
     API_URL = "https://accounts.spotify.com/api/token"
     CLIENT_ID = "fb37da23233d4fe581300fa8d7c4b3a0"
-
+    SPODATA = 'https://api.spotify.com'
 
     WEBDRIVER = "./Data/chromedriver.exe"
     COOKIES = "./Data/cookies.pk1"
 
 
-
     def authorize(self):
-
-
-        
         #Surpress Console Output 
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -42,55 +39,35 @@ class spotifyClient():
 
 
         #create url and open url
-        scopes = 'user-library-modify user-modify-playback-state playlist-modify-private'
+        scopes = 'playlist-modify-private playlist-read-collaborative user-read-private user-library-modify playlist-modify-public user-library-read playlist-read-private'
         url =  self.SPOTIFY_AUTH_URL + "?client_id=" + self.CLIENT_ID + '&response_type=token' + '&redirect_uri=' + urllib.parse.quote(self.REDIR_URL.encode("utf-8")) + '&scope=' + urllib.parse.quote(scopes.encode("utf-8"))
-        driver.get(url)
         
+       
+        driver.get(url)
+
         #while we not at the url
-        while ("access_token" not in driver.current_url):
+        while ("access_token=" not in driver.current_url):
             pass
         else:       #when we at the url
             parse = urllib.parse.urlparse(driver.current_url)
-            access_token = parse.fragment[13:parse.fragment.find("&")]          #get the accesstoken
+            self.access_token = parse.fragment[13:parse.fragment.find("&")]          #get the accesstoken
             pickle.dump(driver.get_cookies(), open(self.COOKIES,"wb"))          #save cookies
-            #driver.close()                                                      #close driver
+            driver.close()                                                      #close driver
                 
-        bytearray([])
-        print(access_token)
-        print(base64.b64encode(access_token.encode()))
 
-        #driver.get(self.API_URL, headers= { 'Authorization': 'Bearer ' + access_token})
-        #repsonse = requests.post(self.API_URL, headers= { 'Authorization': 'Bearer ' + access_token})
 
-        #print(repsonse)
-        
-        '''
-        payload = {
-            'username': 'xxxx',
-            'password': 'xxxx'
-        }
 
-        r = requests.Session()
 
-        
-        print(url)
-        respo = requests.get( 
-            url,
-            headers={
-                'client_id': clientInfo[0]
-            }
-        )
 
-        print(respo.url)
+    def getData(self, url_ented, inquery = None):
+        headers = {'Authorization': 'Bearer %s' % self.access_token }
+        response = requests.get(self.SPODATA + url_ented, headers=headers, params=inquery)        #get data from spotify
+        return response.json()
     
-        respo = r.post(respo.url, data=payload)
-       
-        #print(urllib.parse.urlparse(respo.url))
-        print("------------------")
+        
+        
+                
 
-        #print(url)
-        #print(request.status)
-        '''
 
 
 if __name__ == "__main__":
